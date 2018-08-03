3a344b68493744784d56c10853273a54de2c995e
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-08-04 00:33:19 +0300

    Added CheckResult class

---
+++ convenience/src/main/java/com/visuality/convenience/CheckResult.java
@@ -0,0 +1,24 @@
+package com.visuality.convenience;
+
+public class CheckResult {
+
+    private String[] allowedPermissions;
+
+    public String[] getAllowedPermissions() {
+        return this.allowedPermissions;
+    }
+
+    private String[] blockedPermissions;
+
+    public String[] getBlockedPermissions() {
+        return this.blockedPermissions;
+    }
+
+    public CheckResult(
+            String[] allowedPermissions,
+            String[] blockedPermissions
+    ) {
+        this.allowedPermissions = allowedPermissions;
+        this.blockedPermissions = blockedPermissions;
+    }
+}
